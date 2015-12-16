a326d6315f815eb2ce5d5f94a8d4005e77a7fe3f
Mr Snow <snow@hol.ly>
2015-12-16 11:35:00 +1100

    #18 Implement LDAP authorization as a Provider to the new Authorization module.

---
+++ ldap_authorization/src/Plugin/authorization/provider/LDAPAuthorizationProvider.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @AuthorizationProvider(
+ *   id = "ldap_provider",
+ *   label = @Translation("LDAP Authorization provider"),
+ *   description = @Translation("LDAP provider to the Authorization API.")
+ * )
+ */
+class LDAPAuthorizationProvider extends BackendPluginBase {
+}
