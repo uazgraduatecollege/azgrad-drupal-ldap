e023fff72d87f92f53af6019158ae1b272f51688
Hendrik Grahl <hendrik@grahl.ch>
2017-09-03 15:46:58 +0200

    General improvements to Docker example setup and fixes in LDIF file.

--- docs/hogwarts/docker.sh
+++ docs/hogwarts/docker.sh
@@ -6,19 +6,31 @@ LDIF_FILE=hogwarts.people.ldif
 SLAPD=slapd
 DOCKER_PORT=9389
 DOCKER_NAME=hogwarts_ldap
+DOCKER_IP=127.0.0.1
 
-# Kill previous docker
-LDAP_CID=`docker ps --filter "name=${DOCKER_NAME}" --format "{{.ID}}"`
-echo LDAP_CID $LDAP_CID
+read -p "Bind METHOD ([service_account], anon_user): " METHOD
+METHOD=${METHOD:-service_account}
+
+echo "Stopping all LDAP docker instances"
+array=( service_account anon_user )
+for i in "${array[@]}"
+do
+    CID_SERVICE=`docker ps --filter "name=${DOCKER_NAME}.${i}" --format "{{.ID}}"`
+    if [ $CID_SERVICE ]
+        then
+            docker stop $CID_SERVICE
+    fi
+done
+
+LDAP_CID=`docker ps -a --filter "name=${DOCKER_NAME}.${METHOD}" --format "{{.ID}}"`
 if [ $LDAP_CID ]
 	then
-		echo "Stopping $DOCKER_NAME $LDAP_CID"
-		docker stop $LDAP_CID
+		echo "Removing existing $DOCKER_NAME with $METHOD"
 		docker rm $LDAP_CID
 fi
 
-echo "Starting $DOCKER_NAME"
-LDAP_CID=$(docker run -e LDAP_TLS=false -e LDAP_DOMAIN="$LDAP_DOMAIN" -p $DOCKER_PORT:389 --name=$DOCKER_NAME -d osixia/openldap)
+echo "Starting $DOCKER_NAME with $METHOD"
+LDAP_CID=$(docker run -e LDAP_TLS=false -e LDAP_DOMAIN="$LDAP_DOMAIN" -p $DOCKER_PORT:389 --name="${DOCKER_NAME}.${METHOD}" -d osixia/openldap)
 
 if [ -z "$LDAP_CID" ]
 	then
@@ -27,11 +39,7 @@ if [ -z "$LDAP_CID" ]
 fi
 
 docker cp $LDIF_FILE $LDAP_CID:/$LDIF_FILE
-docker cp grants.ldif $LDAP_CID:/grants.ldif
-
-docker exec -it $LDAP_CID ls
-
-DOCKER_IP=127.0.0.1
+docker cp grants.${METHOD}.ldif $LDAP_CID:/grants.ldif
 
 sleep 3
 echo "Importing LDIF"
@@ -43,5 +51,5 @@ echo ID $LDAP_CID
 
 docker exec -it $LDAP_CID ldapmodify -Y EXTERNAL -H ldapi:/// -f /grants.ldif
 
-echo "Searching LDAP"
-ldapsearch -x -h $DOCKER_IP -p $DOCKER_PORT -b $LDAP_DN -D "cn=hpotter,ou=people,$LDAP_DN" -w pass
+echo "Searching LDAP (user credentials)"
+ldapsearch -x -h $DOCKER_IP -p $DOCKER_PORT -b $LDAP_DN -D "cn=hpotter,ou=people,$LDAP_DN" -w pass dn
