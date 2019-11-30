e3e103287565149da2c1b1904a0d664855f81fb1
micahw156 <micahw156@40138.no-reply.drupal.org>
2019-11-30 19:50:23 +0100

    Issue #3076155 by micahw156: Multiple problems with LdapHelpRedirect

---
+++ ldap_authentication/tests/src/Kernel/RedirectTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\Tests\ldap_authentication\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\ldap_authentication\Controller\LdapHelpRedirect;
+
+/**
+ * @group ldap
+ */
+class RedirectTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'externalauth',
+    'ldap_servers',
+    'ldap_authentication'
+  ];
+
+  protected function setUp() {
+    parent::setUp();
+    $this->installConfig('ldap_authentication');
+  }
+
+  /**
+   * Test the help controller.
+   *
+   * TrustedRedirectResponse does not actually validate the url.
+   */
+  public function testRedirectController(): void {
+    $controller = LdapHelpRedirect::create($this->container);
+
+    $config = $this->config('ldap_authentication.settings');
+    $config ->set('ldapUserHelpLinkUrl', 'https://www.example.com/123.html');
+    $config ->save();
+    $redirect = $controller->redirectUrl();
+    self::assertEquals('https://www.example.com/123.html', $redirect->getTargetUrl());
+    self::assertEquals(0, $redirect->getAge());
+    self::assertEquals(0, $redirect->getCacheableMetadata()->getCacheMaxAge());
+  }
+
+}
