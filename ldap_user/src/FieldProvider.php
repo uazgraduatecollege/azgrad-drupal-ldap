1042b9d08c1898027d63f112d83b6e7fb0cd9236
roderik <roderik@8841.no-reply.drupal.org>
2019-11-30 20:01:38 +0100

    Issue #3092008 by roderik: Possible fix to "leaked metadata" exception

--- ldap_user/src/FieldProvider.php
+++ ldap_user/src/FieldProvider.php
@@ -232,8 +232,12 @@ class FieldProvider implements LdapUserAttributesInterface {
    */
   private function addPuidFields(): void {
     $url = Url::fromRoute('entity.ldap_server.collection');
+    // A plain $url->toString() call in some places (early in the request)
+    // can cause Drupal to throw a 'leaked metadata' exception. To prevent
+    // toString() from handling any metadata in the background, we pass TRUE.
+    $url_string = $url->toString(TRUE)->getGeneratedUrl();
     $tokens = [
-      '%edit_link' => Link::fromTextAndUrl($url->toString(), $url)->toString(),
+      '%edit_link' => Link::fromTextAndUrl($url_string, $url)->toString(),
       '%sid' => $this->server->id(),
     ];
 
